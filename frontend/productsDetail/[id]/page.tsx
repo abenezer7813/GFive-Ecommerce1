b1598a6e1e763362e782e4c7366188194d922878
zekarias <zekariasdev@gmail.com>
2025-12-26 15:12:32 +0300

    product's detail added

---
+++ frontend/productsDetail/[id]/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { use } from "react";
+import ProductDetail from "./ProductDetail";
+import NavBar from "@/app/components/NavBar";
+
+type Props = { params: Promise<{ id: string }> };
+
+export default function ProductDetailPage({ params }: Props) {
+  return (
+    <div className='min-h-screen bg-gray-50'>
+      <NavBar/>
+      {/* Pass params to ProductDetail */}
+      <ProductDetail params={params} />
+    </div>
+  );
+}
