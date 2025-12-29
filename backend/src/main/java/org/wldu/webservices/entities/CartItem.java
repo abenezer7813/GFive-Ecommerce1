b294de9226c1f922705f4dd2523f8943231703e4
ZekariasW-Dev <zekarias.w.dev@gmail.com>
2025-12-29 19:40:27 +0300

    fix CartItem entities

--- backend/src/main/java/org/wldu/webservices/entities/CartItem.java
+++ backend/src/main/java/org/wldu/webservices/entities/CartItem.java
@@ -1,58 +1,37 @@
-package org.wldu.webservices.enities;
+package org.wldu.webservices.entities;
 
 import jakarta.persistence.*;
 
 @Entity
-@Table(
-        name = "cart_items",
-        uniqueConstraints = @UniqueConstraint(columnNames = {"cart_id", "product_id"})
-)
+@Table(name = "cart_items")
 public class CartItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private int quantity;
+
     @ManyToOne
-    @JoinColumn(name = "cart_id", nullable = false)
+    @JoinColumn(name = "cart_id")
     private Cart cart;
 
     @ManyToOne
-    @JoinColumn(name = "product_id", nullable = false)
+    @JoinColumn(name = "product_id")
     private Product product;
 
-    @Column(nullable = false)
-    private int quantity;
-
-
-    public CartItem() {
-
-    }
-
-    public CartItem(Cart cart, Product product, int quantity) {
-        this.cart = cart;
-        this.product = product;
-        this.quantity = quantity;
-    }
-
-    public CartItem(Long id, Cart cart, Product product, int quantity) {
-        this.id = id;
-        this.cart = cart;
-        this.product = product;
-        this.quantity = quantity;
-    }
-
-
-
+    // ===== Getters & Setters =====
     public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+
+    public int getQuantity() { return quantity; }
+
+    public void setQuantity(int quantity) { this.quantity = quantity; }
 
     public Cart getCart() { return cart; }
+
     public void setCart(Cart cart) { this.cart = cart; }
 
     public Product getProduct() { return product; }
-    public void setProduct(Product product) { this.product = product; }
 
-    public int getQuantity() { return quantity; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
+    public void setProduct(Product product) { this.product = product; }
 }
