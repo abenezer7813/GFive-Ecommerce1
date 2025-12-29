2194ddbde9f6b707f50e3ddad586b76deeb7fb05
Abenezer Tariku <abenimom1994@gmail.com>
2025-12-30 02:49:08 +0300

    cart added

--- backend/src/main/java/org/wldu/webservices/dto/cart/CartResponseDTO.java
+++ backend/src/main/java/org/wldu/webservices/dto/cart/CartResponseDTO.java
@@ -5,11 +5,29 @@ import lombok.*;
 import java.math.BigDecimal;
 import java.util.List;
 
-@Getter @AllArgsConstructor
+
 public class CartResponseDTO {
 
     private Long cartId;
     private List<CartItemResponseDTO> items;
     private BigDecimal totalPrice;
+
+    public CartResponseDTO(Long cartId, List<CartItemResponseDTO> items, BigDecimal totalPrice) {
+        this.cartId = cartId;
+        this.items = items;
+        this.totalPrice = totalPrice;
+    }
+
+    public Long getCartId() {
+        return cartId;
+    }
+
+    public List<CartItemResponseDTO> getItems() {
+        return items;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
 }
 
