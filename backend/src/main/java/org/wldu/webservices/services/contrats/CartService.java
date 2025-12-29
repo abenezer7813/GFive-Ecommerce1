25e45cefc420f566182a4cc92512047de56e6279
ZekariasW-Dev <zekarias.w.dev@gmail.com>
2025-12-29 19:42:11 +0300

    fix interface of CartService

--- backend/src/main/java/org/wldu/webservices/services/contrats/CartService.java
+++ backend/src/main/java/org/wldu/webservices/services/contrats/CartService.java
@@ -1,11 +1,7 @@
 package org.wldu.webservices.services.contrats;
 
-
 import org.wldu.webservices.auths.User;
 
 public interface CartService {
-
     void addToCart(User user, Long productId, int quantity);
-
 }
-
