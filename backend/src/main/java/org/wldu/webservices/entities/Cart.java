0772c39735a400bf5c317f1fe7c8040e6fb039ee
ZekariasW-Dev <zekarias.w.dev@gmail.com>
2025-12-29 19:35:32 +0300

    fix Cart entities

--- backend/src/main/java/org/wldu/webservices/entities/Cart.java
+++ backend/src/main/java/org/wldu/webservices/entities/Cart.java
@@ -1,7 +1,6 @@
 package org.wldu.webservices.entities;
 
 import jakarta.persistence.*;
-import lombok.*;
 import org.wldu.webservices.auths.User;
 
 import java.util.ArrayList;
@@ -9,9 +8,6 @@ import java.util.List;
 
 @Entity
 @Table(name = "carts")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Cart {
 
     @Id
@@ -19,13 +15,21 @@ public class Cart {
     private Long id;
 
     @OneToOne
-    @JoinColumn(name = "user_id", unique = true)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
 
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CartItem> items = new ArrayList<>();
 
-    // ===== Helper Methods =====
+    // ===== Getters & Setters =====
+    public Long getId() { return id; }
+
+    public User getUser() { return user; }
+
+    public void setUser(User user) { this.user = user; }
+
+    public List<CartItem> getItems() { return items; }
+
     public void addItem(CartItem item) {
         items.add(item);
         item.setCart(this);
